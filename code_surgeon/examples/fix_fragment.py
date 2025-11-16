943ea88a2fec1505440ca61f6d83915395320deb
WilderSchwarz <herzengine@gmail.com>
2025-11-15 22:59:39 -0600

    chore: exclude code_surgeon and prompts from version control

--- code_surgeon/examples/fix_fragment.py
+++
@@ -1,2 +0,0 @@
-# Only the body between markers, correctly indented by the CLI when inserted.
-return int(a) + int(b)
--- code_surgeon/examples/target.py
+++
@@ -1,7 +0,0 @@
-def add(a, b):
-    # BEGIN:hotfix_area
-    return a + b  # buggy? suppose we need clamp or type cast
-    # END:hotfix_area
-
-def other():
-    print("untouched")
